a964d48e803b21148210b7fca556be0313d37f13
Cody Coleman <colemanc@mit.edu>
2013-01-23 17:26:59 -0500

    Starting on second scenario, but taking a break

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
 
+  def similar
+    @movie = Movie.find(params[:id])
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
+  get 'movies/similar/:id', to: 'movies#similar'
   root :to => redirect('/movies')
 end
